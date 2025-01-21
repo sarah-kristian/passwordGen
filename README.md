d47d218c22e2835c884357f11e533a47f137a721
Phredd <sarahkrisperry@gmail.com>
2025-01-21 11:09:29 -0330

    Simplify argument parsing and enhance help functionality
    - Merged parseArguments and handleArguments into a single function handleArguments to streamline the code.
    - Enhanced clarity of the help message to improve user guidance.
    - Added handling for cases where no arguments are provided, defaulting to showing the help message.
    - Enhanced clarity of README

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
 CLI Password Generator
-A basic command line password generator (all lowercase)
+  This tool generates a random password consisting of lowercase letters. 
+  You can specify the length of the password using the '--length' option. 
+  If no length is provided, the default password length is 8 characters.
 
 Instructions
-Run the app with either npx getPassword or npm ./index.js
+  Run the app with either npx getPassword or npm ./index.js
--- index.js
+++ index.js
@@ -3,8 +3,11 @@
 const process = require('node:process');
 const args = process.argv.slice(2);
 
+
 /**
  * Generates a random character
+ * 
+ * @returns string of random lower case letters
  */
 
 function getRandomCharacter() {
@@ -12,7 +15,6 @@ function getRandomCharacter() {
     let randomIndex = Math.floor(Math.random() * alphabet.length);
     return alphabet[randomIndex];
   }
-
 
 
 /**
@@ -44,75 +46,78 @@ function generatePassword(passwordLength = 8) {
  * Prints the help message
  */
 function printHelpMessage() {
-    console.log(`
-Usage: length [integer]
+    let helpMessage = `Usage: [option]
 
-
-Example:
-  length 5
-  Result: abcde
-    `);
+    Options:
+      --length [integer]    Generate a password of specified length. Default is 8.
+                            Example: --length 5
+                            Result: abcde
+    
+      --help                Display this help message and exit.
+    
+    Description:
+      This tool generates a random password consisting of lowercase letters. 
+      You can specify the length of the password using the '--length' option. 
+      If no length is provided, the default password length is 8 characters.
+      
+    Examples:
+      node passwordGen.js --length 12
+      Result: Random 12 character password.
+    
+      node passwordGen.js --help
+      Displays help information.
+    
+    Notes:
+      - If an invalid length is provided after '--length', the application will revert to the default length.
+      - The password is purely alphabetical and does not include numbers or special characters.
+        `;
+    
+        console.log(helpMessage);
 }
 
 
 /**
  * Handles the arguments provided to the application by the user.
  * 
- * @param {string[]} userArguments The arguments provided by the user
+ * @param {string[]} args The arguments provided by the user
  */
 
 
-function parseArguments(args) {
+function handleArguments(args) {
     // const options = {
     //     length: 8, // default length
     // };
 
-    let length = 8;
+    if (args.length === 0) {
+      printHelpMessage();
+      return;
+  }
 
     for (let i = 0; i < args.length; i++) {
         switch (args[i]) {
             case '--length':
+                let length = 8;
                 if (i + 1 < args.length && !isNaN(parseInt(args[i + 1], 10))) {
                     length = parseInt(args[i + 1], 10);
+                    const result = generatePassword(length);
+                    console.log(`Password: ${result}`);
                     i++;
                 }else{
-                    console.log('error: expecting an integer after --length. A default length of 8 will be used.')
+                    console.log('error: Expected an integer after --length. A default length of 8 was used.')
                 } 
                 break;
 
+            case '--h':
             case '--help':
                 printHelpMessage();
                 break;
 
             default : 
                 console.log('argument not understood. Type --help for help.')
+                return;
         }
     }
-
-    return length;
 }
 
-
-function handleArguments(userArguments) {
-    length = parseArguments(args);
-
-    // if (userArguments.includes('--length')) {
-    //     console.log(arguments);
-    //     length = userArguments[0].split(" ");
-    // }
-    // // if (userArguments.length === 1) {
-    // //     userArguments = userArguments[0].split(" ");
-    // // }
-
-    try {
-        const result = generatePassword(userArguments);
-        console.log(`Result: ${result}`);
-    }
-    catch (error) {
-        console.error(`Error: ${error.message}`);
-        printHelpMessage();
-    }
-
-}
 
 handleArguments(args);
